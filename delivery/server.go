5e1e3bbf6f9acc3b04a1a813821ef62eead40b39
Agung Panjimasjaya <agungpanji2950@gmail.com>
2023-10-26 16:51:49 +0700

    add: middleware log

--- delivery/server.go
+++ delivery/server.go
@@ -1,1 +1,54 @@
 package delivery
+
+import (
+	"fmt"
+	"mnc/config"
+	"mnc/delivery/controller"
+	"mnc/delivery/middleware"
+	"mnc/manager"
+	exceptions "mnc/utils/exception"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type Server struct {
+	useCaseManager manager.UsecaseManager
+	engine         *gin.Engine
+	host           string
+	log            *logrus.Logger
+}
+
+func (s *Server) Run() {
+	s.initController()
+	err := s.engine.Run(s.host)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) initController() {
+	s.engine.Use(middleware.LogRequestMiddleware(s.log))
+	// semua controller disini
+	controller.NewBankController(s.useCaseManager.BankUsecase(), s.engine)
+	controller.NewCustomerController(s.useCaseManager.CustomerUsecase(), s.engine)
+	controller.NewMerchantController(s.useCaseManager.MerchantUsecase(), s.engine)
+	controller.NewTransactionController(s.useCaseManager.TransactionUsecase(), s.engine)
+}
+
+func NewServer() *Server {
+	cfg, err := config.NewConfig()
+	exceptions.CheckError(err)
+	infraManager, _ := manager.NewInfraManager(cfg)
+	repoManager := manager.NewRepoManager(infraManager)
+	useCaseManager := manager.NewUseCaseManager(repoManager)
+
+	engine := gin.Default()
+	host := fmt.Sprintf("%s:%s", cfg.ApiHost, cfg.ApiPort)
+	return &Server{
+		useCaseManager: useCaseManager,
+		engine:         engine,
+		host:           host,
+		log:            logrus.New(),
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/google/uuid v1.3.1
 	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/crypto v0.14.0
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
